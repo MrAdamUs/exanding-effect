4cfafa538aa830568d69ee516c14a7a88f16aaf3
MrAdam <adam@mradam.us>
2021-05-16 13:36:25 -0500

    done the project

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-console.log('Start your project ');
+const panels = document.querySelectorAll('.panel');
+
+panels.forEach((panel) => {
+  panel.addEventListener('click', () => {
+    removeActiveClass();
+    panel.classList.add('active');
+  });
+});
+
+function removeActiveClass() {
+  panels.forEach((panel) => {
+    panel.classList.remove('active');
+  });
+}
